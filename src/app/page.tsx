95b22c78012c20b0c712c2c657a50725033e27a9
joy016 <jhoylejas@gmail.com>
2024-08-23 15:06:18 +0800

    simple to do app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,200 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  Modal,
+  Paper,
+  TextareaAutosize,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { pink } from '@mui/material/colors';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import { useEffect, useState } from 'react';
+import { TODO_DATA } from '@/lib/constant';
+
+interface Todo {
+  item: string;
+  checked: boolean;
+}
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '20px',
+};
 
 export default function Home() {
+  const [todoItems, setTodoItems] = useState<Todo[]>([]);
+  const [open, setOpen] = useState(false);
+  const [newTodo, setNewTodo] = useState({
+    item: '',
+    checked: false,
+  });
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const itemStored = localStorage.getItem('todoItems');
+    if (itemStored) {
+      setTodoItems(JSON.parse(itemStored));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (todoItems.length > 0)
+      localStorage.setItem('todoItems', JSON.stringify(todoItems));
+  }, [todoItems]);
+
+  const handleCheck = (index: any) => {
+    const newItems = [...todoItems];
+    newItems[index].checked = !newItems[index].checked;
+    setTodoItems(newItems);
+    setScore(score);
+  };
+
+  const handleDelete = (index: any) => {
+    setTodoItems((prevValue) => prevValue.filter((_, i) => i !== index));
+  };
+
+  const handleChangeTodo = (event: any) => {
+    setNewTodo({
+      ...newTodo,
+      item: event.target.value,
+    });
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.item.trim() != '') {
+      setTodoItems((prevValue) => {
+        const updateItems = [...prevValue, newTodo];
+        return updateItems;
+      });
+      setNewTodo({ item: '', checked: false });
+      setOpen(false);
+    }
+  };
+
+  const handleOpenModal = () => setOpen(true);
+  const handleCloseModal = () => setOpen(false);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '5rem',
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            padding: '1em',
+            width: { xs: '80%', lg: '50%' },
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            gap: '30px',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              padding: '0 20px',
+              justifyContent: 'space-between',
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <Typography>My Daily Todo Task</Typography>
+            <Typography>Score: {score}</Typography>
+          </Box>
+          <FormGroup>
+            {todoItems.map((item, index) => {
+              return (
+                <Box
+                  key={index}
+                  sx={{ display: 'flex', justifyContent: 'space-between' }}
+                >
+                  <FormControlLabel
+                    key={index}
+                    control={
+                      <Checkbox
+                        sx={{
+                          color: pink[800],
+                          '&.Mui-checked': {
+                            color: pink[600],
+                          },
+                        }}
+                        onChange={() => handleCheck(index)}
+                      />
+                    }
+                    sx={{
+                      textDecoration: item.checked ? 'line-through' : 'none',
+                    }}
+                    label={item.item}
+                  />
+                  <IconButton
+                    onClick={() => handleDelete(index)}
+                    aria-label="delete"
+                    sx={{
+                      color: pink[800],
+                      '&.Mui-checked': {
+                        color: pink[600],
+                      },
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Box>
+              );
+            })}
+          </FormGroup>
+          <Button
+            onClick={handleOpenModal}
+            variant="outlined"
+            startIcon={<AddIcon />}
+            sx={{
+              width: '80%',
+              margin: '0 auto',
+            }}
+          >
+            Add Todo
+          </Button>
+          <Modal
+            open={open}
+            onClose={handleCloseModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <TextField
+                id="outlined-basic"
+                label="Add Todo"
+                variant="outlined"
+                fullWidth
+                onChange={handleChangeTodo}
+                value={newTodo.item}
+              />
+              <Button variant="contained" onClick={handleAddTodo}>
+                Save Todo
+              </Button>
+            </Box>
+          </Modal>
+        </Paper>
+      </Box>
+    </>
   );
 }
---
+++ src/lib/constant.ts
@@ -0,0 +1,20 @@
+const TODO_DATA = [
+  {
+    item: 'Watch Css Tutorial',
+    checked: false,
+  },
+  {
+    item: 'Drink 2 Liters of water',
+    checked: false,
+  },
+  {
+    item: 'Answer 1 Problem in hackerank',
+    checked: false,
+  },
+  {
+    item: 'Practice Typing atleast 20 Mins',
+    checked: false,
+  },
+];
+
+export { TODO_DATA };
